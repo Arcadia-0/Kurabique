09f99d49509e82f18090c4c6f877cc1c4f4cbdad
Arcadia-0 <business.ydeniz@gmail.com>
2025-01-14 10:29:31 +0300

    TheCookieShop

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sourceSans: ['"Source Sans Pro"', 'sans-serif'],
+    },
+    colors:{
+     primary:'#FFD700'
+    }
+  },
+  plugins: [],
+}
+ }
