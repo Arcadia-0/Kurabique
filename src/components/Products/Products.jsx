9e1fc05d3e7dd5915648694a5fb0bad3ab2a8ead
Arcadia-0 <business.ydeniz@gmail.com>
2025-01-22 19:47:35 +0300

    its better without absolute xD

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,18 +1,82 @@
-import card from "../../assets/products/card.png";
 import cookie from "../../assets/products/cookie.png";
+import cake from "../../assets/products/cake.png";
+import chocolate from "../../assets/products/chocolate.png";
+import gift from "../../assets/products/gift.png";
+import button from "../../assets/products/button.png";
+
+
+const products = [
+  {
+    id: 1,
+    image: cookie,
+    title: "KURABİYE",
+    description: "Farklı tatlar ve şekillerde özenle hazırlanan, her lokması mutluluk dolu taptaze kurabiyeler.",
+    button: button,
+    
+  },
+  {
+    id: 2,
+    image: cake,
+    title: "KEK",
+    description: "Yumuşacık dokusu ve zengin tatlarıyla her anınıza lezzet katacak ev yapımı kekler.",
+    button: button,
+    
+  },
+  {
+    id: 3,
+    image: chocolate,
+    title: "ÇİKOLATA",
+    description: "Özel günlerinizi unutulmaz kılacak, şık ve zarif hediye seçenekleri. Sevdiklerinize tatlı sürprizler!",
+    button: button,
+   
+  },
+  {
+    id: 4,
+    image: gift,
+    title: "HEDİYELİK",
+    description: "Zengin kakao tadı ve kaliteli malzemelerle hazırlanan, damaklarda iz bırakan çikolatalar.",
+    button: button,
+   
+  },
+];
+
+const ProductCard = ({ image, title, description, button}) => {
+  return (
+    <div
+      className="bg-primary rounded-lg shadow-lg transition-transform transform hover:shadow-2xl hover:translate-y-2"
+    >
+      <div className="flex flex-col items-center justify-center pt-6">
+       
+        <div className="bg-primary rounded-full overflow-hidden mb-4">
+          <img src={image} alt={title} className="w-full h-full object-cover" />
+        </div>
+        <h1 className="text-xl font-semibold text-secondary mb-2">{title}</h1>
+        <p className="text-secondary text-sm text-center mb-4">
+          {description}
+        </p>
+        <img src={button} alt="Button" className="w-32 mx-auto" />
+      </div>
+    </div>
+  );
+};
 
 const Products = () => {
   return (
-    <div className="container mx-auto pt-6 relative flex">
-     
-      <img src={card} alt="Card"  />
-
-     
-      <img src={cookie} alt="" className="absolute top-14 left-4" />
-      <h1 className="text-white text-4xl font-bold absolute top-14 left-80 ">
-        Kurabiye
-      </h1>
-      <p className="absolute bottom-32 left-40">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, saepe.</p>
+    <div className="container mx-auto mt-8 ">
+      {/* Grid container for the cards */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+        {/* render card */}
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            image={product.image}
+            title={product.title}
+            description={product.description}
+            button={product.button}
+            bgColor={product.bgColor}
+          />
+        ))}
+      </div>
     </div>
   );
 };
